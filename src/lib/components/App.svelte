3352f25e5fe570b2c5ad8cb70f56a680ec2ee46e
hapqe <konrad@hapkem.com>
2024-10-03 21:31:04 +0200

    refactored

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,39 +1,41 @@
 <script context="module">
-  import { writable } from "svelte/store";
+    import {writable} from "svelte/store";
 
-  export const scrollState = writable(undefined);
+    /** @type {import('svelte/store').Writable<number | undefined>} */
+    export const scrollState = writable(undefined);
 </script>
 
 <script>
-  import { Canvas } from "@threlte/core";
-  import Scene from "./Scene.svelte";
-  import Header from "./Header.svelte";
-  import About from "./About.svelte";
+    import {Canvas} from "@threlte/core";
+    import Scene from "./Scene.svelte";
+    import Header from "./Header.svelte";
+    import About from "./About.svelte";
 </script>
 
 <div id="canvas-frame">
-  <Canvas>
-    <Scene />
-  </Canvas>
+    <Canvas>
+        <Scene/>
+    </Canvas>
 </div>
 
 <div id="content">
-  <Header />
-  <About />
+    <Header/>
+    <About/>
 </div>
 
 <style>
-  #canvas-frame {
-    position: fixed;
-    height: 100svh;
-    width: 100svw;
-    pointer-events: auto;
-    z-index: 0;
-  }
-  #content {
-    text-align: center;
-    position: relative;
+    #canvas-frame {
+        position: fixed;
+        height: 100svh;
+        width: 100svw;
+        pointer-events: auto;
+        z-index: 0;
+    }
 
-    pointer-events: none;
-  }
+    #content {
+        text-align: center;
+        position: relative;
+
+        pointer-events: none;
+    }
 </style>
